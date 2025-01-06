62bfa74e338f73aa5f8b951b417ac1938ca0ff00
SGOBI-CODER <gobi9965676310@gmail.com>
2025-01-06 08:29:51 +0530

    Add files via upload

---
+++ pyramid.java.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Main
+    {
+        public static void main (String args[])
+        {
+            for(int row=1;row<=5;row++)
+            {
+                for(int col=1;col<=5-row;col++)
+                {
+                    System.out.print(" ");
+                    
+                }
+                  for(int col=1;col<=((2*row)-1);col++)
+                {
+                    System.out.print("*");
+                    
+                }
+                System.out.println();
+                
+            
+            }
+        }
+    }
